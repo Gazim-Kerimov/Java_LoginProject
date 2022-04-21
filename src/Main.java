71def82801d430d6d978bc0bad9102ebf54de122
Gazim-Kerimov <gazim94@mail.ru>
2022-04-21 13:21:56 +0300

    Added creation of 3 logins

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        LoginCreator creator = new LoginCreator();
+        try {
+            for(int i = 0; i < 3; i++){
+                creator.AddLogin();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
 
     }
 }
